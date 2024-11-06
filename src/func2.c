47c6b149bd4091b0bb409045aaff437a48204f44
Gyounggeun Bae <bkg6638@naver.com>
2024-11-06 13:52:42 +0900

    myapp 2

---
+++ src/func2.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "func2.h"
+
+
+void  goo(){
+
+    printf("goo\n");
+}
--- src/main.c
+++ src/main.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include "func1.h"
-
+#include "func2.h"
 
 int main(){
 
 
     foo();
+    goo();
     printf("main\n");
-
+    
 }
